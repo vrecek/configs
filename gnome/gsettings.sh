7788c2672db678da5b2430800e3331626bdb81fb
vrecek <vrecek@proton.me>
2024-02-26 01:06:26 +0100

    gsettings

---
+++ gnome/gsettings.sh
@@ -0,0 +1,4 @@
+gsettings set org.gnome.desktop.interface clock-show-seconds true
+gsettings set org.gnome.desktop.interface clock-show-weekday true
+
+gsettings set org.gnome.desktop.sound allow-volume-above-100-percent true
